aba03533a7b8a005bef2fb6a6def3a390db68f3b
Chandan Kumar <chandanchaudhary@outlook.com>
2019-05-15 19:57:13 +0530

    Remove auto_now false for pub_date field

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200, blank=True, null=True)
-    pub_date = models.DateTimeField(("date published"), auto_now=True)
+    pub_date = models.DateTimeField(("date published"), auto_now=False)
 
     def __str__(self):
         return self.question_text
